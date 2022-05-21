fe25a12bd6aa5c6a23c54dd7c97136403f753128
ryanspeciale <difficultoctet@gmail.com>
2022-05-21 17:45:06 -0400

    layout

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer';
+import TopNav from './TopNav';
+
+
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <TopNav />
+            <main>{ children }</main>
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <CssBaseline>
+        <Layout>
+          <Component { ...pageProps }/>
+        </Layout>
+      </CssBaseline>
+    </div>
+  )
 }
 
 export default MyApp
